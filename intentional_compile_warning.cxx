68c3cebf842aa8a13d659fec428fd85ca3a24307
David Cole <david.cole@kitware.com>
2011-09-13 16:56:47 -0400

    Add more lines of intentionally warning code
    Some compilers do not warning about signed/unsigned mismatch by
    default. We want to trigger warnings by default when this file
    is compiled.

--- intentional_compile_warning.cxx
+++ intentional_compile_warning.cxx
@@ -2,7 +2,10 @@
 
 int main(int argc, const char* argv[])
 {
+  unsigned char c = 65537; // a constant clearly outside the range of a 'char'
   unsigned int i = 0; // "i<argc" should produce a "signed/unsigned comparison" warning
+  unsigned int j = 0; // j is never used after its assignment
+  unsigned int k; // k is neither initialized nor used...
   for (; i<argc; ++i)
   {
     fprintf(stdout, "%s\n", argv[i]);

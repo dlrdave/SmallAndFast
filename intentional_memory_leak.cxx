9edc1171a8a9313d25b732f855285fce3828c2f9
David Cole <DLRdave@aol.com>
2015-05-08 10:28:15 -0400

    - Commit removing code from existing files

--- intentional_memory_leak.cxx
+++ intentional_memory_leak.cxx
@@ -3,18 +3,6 @@
 #include <stdlib.h>
 
 
-//
-// Define a function, but do not call it from anywhere. No reason, just
-// testing things here, move along. This is not the function you're
-// looking for.
-//
-unsigned int multiply_by_e_and_round(const unsigned int input)
-{
-  double output = 2.718281828459045 * (double) input;
-  return (unsigned int)(output + 0.5);
-}
-
-
 int main(int argc, const char* argv[])
 {
   const char *leak = (const char *) malloc(42); // no free, intentionally
